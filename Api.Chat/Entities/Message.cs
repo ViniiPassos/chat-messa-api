31ab917324ca39121d86b9f430f6173f6c6e5b44
Vinicius Passos <vinicius.pmp19@gmail.com>
2022-11-17 16:50:11 -0300

    api do chat simples

---
+++ Api.Chat/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.Chat.Entities
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public string Date { get; set; }
+        public Message(string name, string body)
+        {
+            Name = name;
+            Body = body;
+            Date = DateTime.Now.ToString("G");
+        }
+    }
+}
